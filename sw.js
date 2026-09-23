b16a2a46b5d8a7a2d9749ea10779ece686fd5db0
ccmatrix501-spec <ccmatrix501@gmail.com>
2026-09-24 03:05:15 +1000

    Refresh web cache marker for v1.8.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mi-tactical-centre-disabled-v1.4.8";
+const CACHE_NAME = "mi-tactical-centre-disabled-v1.8.0";
 self.addEventListener("install", () => { self.skipWaiting(); });
 self.addEventListener("activate", (event) => {
   event.waitUntil(
